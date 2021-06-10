c71e96c3c4efa0cd01b9d580a8d425120f390059
Caio Carnelos <caio.emidio@gmail.com>
2021-06-10 01:02:49 +0100

    heroku fix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  await app.listen(port);
 }
 bootstrap();
